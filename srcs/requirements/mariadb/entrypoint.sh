029ee74e17ad6c56128a66f72b9c0a28f8870947
d4nlog-dev <danieljosias693@gmail.com>
2026-03-03 13:44:20 +0100

    Resolvendo o problema do script nao incializar e criar o banco de dados, o usuario e alterar a pass do root

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,39 +1,41 @@
+#!/bin/sh
 
-#!/bin/bash
-set -e
+MYSQL_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD_FILE)
+MYSQL_PASSWORD=$(cat $MYSQL_PASSWORD_FILE)
 
 MYSQL_DATADIR=/var/lib/mysql
-SOCKET_DIR=/run/mysqld
 
-echo "[INFO] Criando diretório de socket: $SOCKET_DIR"
-mkdir -p "$SOCKET_DIR"
-chown -R mysql:mysql "$SOCKET_DIR"
-chmod 755 "$SOCKET_DIR"
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql $MYSQL_DATADIR
 
-# Inicializa MariaDB se ainda não houver dados
 if [ ! -d "$MYSQL_DATADIR/mysql" ]; then
-    echo "[INFO] Inicializando MariaDB..."
-    mysqld --initialize-insecure --user=mysql --datadir="$MYSQL_DATADIR"
+    echo "Instalando tabelas de sistema do MariaDB..."
+    mariadb-install-db --user=mysql --datadir=$MYSQL_DATADIR > /dev/null
 fi
 
-# Inicia MariaDB em background
-mysqld_safe --datadir="$MYSQL_DATADIR" --socket="$SOCKET_DIR/mysqld.sock" &
-pid="$!"
+if [ ! -d "$MYSQL_DATADIR/$MYSQL_DATABASE" ]; then
+    echo "Iniciando configuração inicial..."
+    mariadbd --user=mysql --skip-networking &
+    pid="$!"
 
-# Espera até o servidor ficar pronto
-echo "[INFO] Aguardando MariaDB iniciar..."
-until mysqladmin ping &>/dev/null; do
-    sleep 1
-done
+    until mariadb-admin ping --silent; do
+        echo "Aguardando MariaDB subir..."
+        sleep 1
+    done
 
-# Cria banco e usuário se não existir
-echo "[INFO] Criando banco e usuário se necessário..."
-mysql <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-    FLUSH PRIVILEGES;
-EOSQL
+    echo "\n\n\n a senha e eesta: $MYSQL_PASSWORD \n\n\n"
+    mariadb -u root << EOF
+        FLUSH PRIVILEGES;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+        FLUSH PRIVILEGES;
+EOF
+    kill "$pid"
+    wait "$pid"
+fi
 
-echo "[INFO] MariaDB iniciado. Mantendo processo em foreground..."
-wait "$pid"
+echo "MariaDB pronto para conexões!"
+exec mariadbd --user=mysql
