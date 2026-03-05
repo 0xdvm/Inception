77d3907d2fe83cf05ba6a050dbfe28fbe97e0218
mr_lorddev <danieljosias693@gmail.com>
2026-03-05 21:29:33 +0100

    Tentando resolver o problema do mariadb nao iniciar na porta padrao.

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -36,7 +36,7 @@ if [ "$FIRST_RUN" -eq 1 ]; then
         CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 
         CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '$DB_PASSWORD';
-        GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${MYSQL_USER}'@'%';
+        GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 
         CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
         GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'localhost';
