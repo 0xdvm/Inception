8a8a9d2f8857eef0eceb2278a4138aa3487fe12d
mr_lorddev <danieljosias693@gmail.com>
2026-03-06 12:00:27 +0100

    Instalando o wordpress e criando novos usuarios.

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# # Espera até MariaDB responder
-# until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -e "select 1" >/dev/null 2>&1; do
-#   echo "Aguardando MariaDB..."
-#   sleep 2
-# done
+# Pega os valores que estao nas variaveis de ambientes que apontam para um arquivo
+DB_PASS=$(cat $MYSQL_PASSWORD_FILE)
+WP_ADMIN_PASS=$(cat $WP_ADMIN_PASSWORD_FILE)
+WP_USER_PASS=$(cat $WP_USER_PASSWORD_FILE)
+
+# Espera até MariaDB responder
+until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -e "select 1" >/dev/null 2>&1; do
+  echo "Aguardando MariaDB..."
+  sleep 2
+done
 
 WP_DIR=/var/www/html/wordpress
 #  Cria o o diretorio se nao existir
@@ -16,8 +21,6 @@ cd $WP_DIR
 # Baixa o wordpress e no arquivo wp-config.php nao existir
 if [ ! -f wp-config.php ]; then
 
-    # Busca a senha do bancod e dados atraves da variavel de ambiente.
-    DB_PASS=$(cat $MYSQL_PASSWORD_FILE)
 
     # Baixa os arquivos do wordpress
     # wp core download --allow-root
@@ -31,8 +34,19 @@ if [ ! -f wp-config.php ]; then
     --skip-check \
     --allow-root
 
-    # 
+    # Instala o wordpress com as configuracoes do admin
+    wp core install \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASS" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --allow-root
 
+    # Cria um usuario normal
+    wp user create "$WP_USER" "$WP_USER_EMAIL" \
+    --user_pass=$WP_USER_PASS \
+    --allow-root
 
 fi
 
